e8d7d327b858595fbaeacccead9aa0809fea81d1
Richard Bates <richo225@gmail.com>
2016-10-02 22:12:36 +0100

    Extracts hero list into a service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,11 @@
-import { Component } from "@angular/core";
-import { Hero } from "./hero"
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
 
 // @Component decorator associates metadata with the AppComponent
 @Component({
   selector: "my-app",
+  providers: [HeroService],
   template:`
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -82,10 +71,20 @@ const HEROES: Hero[] = [
   // read the title and hero properties from AppComponent and render them
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Tour of Heroes";
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {};
+
+  ngOnInit(): void {
+   this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { Hero } from "./hero"
+import { HEROES } from "./mock-heroes"
+
+@Injectable()
+// Decorator emits metadata about the hero service
+// Angular may need this to inject other dependencies into the service in the future.
+export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
