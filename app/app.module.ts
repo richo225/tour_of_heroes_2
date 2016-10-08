5ea0f2cabadd5ea3fb24933ad7512778ad4983a3
Richard Bates <richo225@gmail.com>
2016-10-08 23:01:09 +0100

    Refactor HeroDetailComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -27,6 +27,10 @@ import { DashboardComponent } from "./dashboard.component";
         path: "",
         redirectTo: "/dashboard",
         pathMatch: "full"
+      },
+      {
+        path: "detail/:id",
+        component: HeroDetailComponent
       }
     ])
   ],
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
+
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
 
 @Component({
+  moduleId: module.id,
   selector: "my-dashboard",
-  template: "<h3>My Dashboard</h3>"
+  templateUrl: "dashboard.component.html"
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
+  }
+
+  /* Define a heroes array property.
+     Inject the HeroService in the constructor and hold it in a private heroService field.
+     Call the service to get heroes inside the Angular ngOnInit lifecycle hook. */
+
+  gotoDetail(hero: Hero): void {
+    let link = ["/detail", hero.id];
+    this.router.navigate(link);
+  }
+}
